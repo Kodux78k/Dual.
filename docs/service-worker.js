3db33700aed5541e7be9e8790ff669e23b4848eb
Kodux78k <koduxlive@gmail.com>
2025-05-19 00:26:00 -0300

    Atualizar o service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('metapulso-v9').then(cache => {
+    caches.open('metapulso-v10').then(cache => {
       return cache.addAll(['/', '/index.html', '/manifest.json']);
     })
   );
