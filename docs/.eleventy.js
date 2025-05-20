89b76c1951f9c6c668f24d446f256fcdf4b1cfcf
Kodux78k <koduxlive@gmail.com>
2025-05-20 00:23:23 -0300

    Update .eleventy.js

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,3 +1,22 @@
+module.exports = function(eleventyConfig) {
+  // Copia assets, manifest e SW direto pro build
+  eleventyConfig.addPassthroughCopy("docs/assets");
+  eleventyConfig.addPassthroughCopy("docs/manifest.json");
+  eleventyConfig.addPassthroughCopy("docs/service-worker.js");
+
+  return {
+    // input: onde ficam teus MD/templates
+    dir: {
+      input: "docs/pages",
+      layouts: "../layouts",   // cria pasta layouts/ com header/footer
+      output: "docs/_site"
+    },
+    // opcional: ignora arquivos ocultos
+    templateFormats: ["html", "md"]
+  };
+};
+
+
 const Image = require("@11ty/eleventy-img");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const seo = require("eleventy-plugin-seo");
