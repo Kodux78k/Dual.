543da8e092964a08850568a1dd3b33d54ac19dd6
Kodux78k <koduxlive@gmail.com>
2025-06-05 19:46:38 -0300

    Atualizar o configkey.js

--- docs/config/configkey.js
+++ docs/config/configkey.js
@@ -1,5 +1,5 @@
 // config/configkey.js
 const CONFIG = {
-  API_URL: 'https://Kodux78k/github.io/Dual./dual-tube-9-3',
+  API_URL: 'https://Kodux78k.github.io/Dual./dual-tube-9-3',
   AUTH_TOKEN: 'Bearer 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'
 };
